7f07ce67afe8dcf46f262d3e5445b47a8f250a91
kongxiaoxiang <251314155@qq.com>
2020-05-12 21:12:15 +0800

    回顾

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,25 @@
   es6模块化：分别，统一，默认；如何引入，取决该模块如何暴露
  */
 //引入模块
+import '@babel/polyfill' // 包含ES6的高级语法的转换
 import {sum} from './module1'
 import {sub} from './module2'
 import object from './module3'
 import data from './data.json'
+import '../css/index.less'
 
 console.log(sum(1,2))
 console.log(sub(4,2))
 console.log(object.mul(1,2))
 console.log(object.div(4,2))
 console.log(data)
+
+let P = new Promise((resolve)=>{
+  setTimeout(() => {
+    resolve('atguigu')
+  }, 1000);
+})
+P.then(
+  value => {console.log('success',value)},
+  reason => {console.log('fail',reason)}
+)
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,64 @@ const path = require('path');
 module.exports = {
   entry:'./src/js/app.js',
   output:{
-    filename:'app.js',//输出文件名字
-    path:path.resolve(__dirname,'dist/js')
+    filename:'js/app.js',//输出文件名字
+    path:path.resolve(__dirname,'dist')
   },
-  mode:'development' //工作模式
+  mode:'development', //工作模式
+  //module是一个配置对象，对象里面有一个rules属性
+  module:{ //rules专门用于指定一个个loader
+    rules:[
+      //将less编译成sytle标签 
+      {
+        test:/\.less$/, //匹配所有less文件
+        use:[
+          {loader:"style-loader"},//将commonjs的模块翻译成sytle标签
+          {loader:"css-loader"}, //将css翻译成commonjs的一个模块(内存)
+          {loader:"less-loader"} //将less翻译成css(内存)
+        ]
+      },
+     //使用eslint-loader对js进行语法检查
+      {
+       test:/\.js$/, //匹配所有js文件
+       exclude:/node_modules/, //排除node_modules文件
+       loader:'eslint-loader',
+      },
+     //使用babel-loader配合core-js和polyfill对js进行语法转换及兼容性处理
+      {
+       test: /\.js$/,
+       exclude: /node_modules/, 
+       use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                '@babel/preset-env', //开启babel解析环境
+                {
+                  useBuiltIns: 'usage',  // 按需引入(需要使用polyfill)
+                  corejs: { version: 3 }, // 解决warning警告
+                  targets: { // 指定兼容性处理哪些浏览器
+                    "chrome": "58",
+                    "ie": "9",
+                  }
+                }
+              ]
+            ],
+            cacheDirectory: true, // 开启babel缓存
+          }
+        }       
+      },
+     //使用file-loader处理less文件中的图片
+     {
+      test: /\.(png|jpg|gif)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          outputPath:'/imgs', //输出路径
+          name:'[hash:5].[ext]', //文件命名格式
+          publicPath:'/dist/imgs' //加载图片时候的路径
+        }
+      }
+    }
+    ]
+  }
 }
